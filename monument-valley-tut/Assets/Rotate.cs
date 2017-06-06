04866b30d1c50eede2e8d9450f59aeb5286dc231
William Kwan <william3252@gmail.com>
2017-06-06 12:55:07 -0400

    init

---
+++ monument-valley-tut/Assets/Rotate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    private bool rotatingClockwise = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (rotatingClockwise && (transform.eulerAngles.z > 270 || transform.eulerAngles.z <= 0))
+	    {
+            transform.Rotate(0, 0, -1);
+	    }
+	    else if (!rotatingClockwise && transform.eulerAngles.z > 0)
+	    {
+            transform.Rotate(0, 0, 1);
+        
+        }
+
+        if (Input.anyKeyDown)
+	    {
+	        rotatingClockwise = !rotatingClockwise;
+	    }
+	}
+}
